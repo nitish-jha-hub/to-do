243da6a8aa6de897b1c0c2b5a59a79198edccf5a
Nitish Jha <nitishkumar0914@gmail.com>
2023-12-08 00:34:05 +0530

    attribute selector

---
+++ app/form/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../form/styles1.css'
+
+const Page = () => {
+  return (
+    <div>
+        <form action="" id="id1">
+            <label htmlFor="id"> Text Box</label> <br></br>
+            <input type="text" id="id" placeholder='enter any text' />
+            
+
+        </form>
+    </div>
+  )
+}
+
+export default Page
