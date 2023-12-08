07eb0c225b62140cf23fcf48710a1073965d7147
Nitish Jha <nitishkumar0914@gmail.com>
2023-12-09 00:30:28 +0530

    add todo to array, saved and fetch from local

--- app/form/page.js
+++ app/form/page.js
@@ -1,15 +1,30 @@
+// "use client"
 import React from 'react'
-import styles from '../form/styles1.css'
+import '../form/styles1.css'
+import { Metadata } from 'next'
+
+
+export const metadata = {
+  title: '...',
+  description: '...',
+}
 
 const Page = () => {
+
+
+  let route1=()=>{
+    alert("hello")
+  }
   return (
+
     <div>
         <form action="" id="id1">
-            <label htmlFor="id"> Text Box</label> <br></br>
+            <label id="id3" htmlFor="id"> Text Box</label> 
             <input type="text" id="id" placeholder='enter any text' />
             
 
         </form>
+            <button onClick={1}>Window open func</button>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,66 @@
+"use client"
 import styles from './page.module.css'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
+
+
+const todos = ()=>{
+  let list = localStorage.getItem("list")
+  console.log(JSON.parse(list))
+  
+  return JSON.parse(list)
+
+}
 
 export default function Home() {
+  
+  const [text, setText] = useState("")
+  const [todolist, setTodolist] = useState(todos())
+  
+  const clearall = (e)=>{
+    e.preventDefault();
+    setTodolist([])
+    localStorage.setItem("list" , JSON.stringify(todolist))
+  }
+  useEffect(() => {
+    setTodolist(todos())
+  }, [text])
+  
+  
+
+  const addfunction = (e) => {
+    e.preventDefault();
+    if (!text) {      
+    } else {
+      localStorage.setItem('list', JSON.stringify([...todolist,text]))
+      setText("")
+    }
+  }
+
+  const removeitem = (todolist,id)=>{
+  
+  }
+
+
+
   return (
     <main className={styles.main}>
-      jfjehjh
+      <Link href="/form">form</Link>
+      <h1>Home Page :Todo</h1>
+
+      <form>
+        <label htmlFor="inputid">add task</label> <br />
+        <input type="text" id='inputid' onChange={e => setText(e.target.value)} value={text} />
+        <button onClick={addfunction} >Add</button>
+        <button onClick={clearall}>clear all</button>
+      </form>
+      <ol>
+      {todolist.map((key)=>{
+        return <li> {key} </li>
+      })}
+      </ol>
+      
     </main>
   )
 }
